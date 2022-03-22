e4e4dcb4a902008f10658fdba4b5988b6e86ef4a
nikolaj <n.d.maximov@gmail.com>
2022-03-22 16:21:14 +0300

    initial commit

---
+++ src/main/java/tech/maximov/bots/expense/TelegramExpenseBotApplication.java
@@ -0,0 +1,13 @@
+package tech.maximov.bots.expense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramExpenseBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramExpenseBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/maximov/bots/expense/TelegramExpenseBotApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.maximov.bots.expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramExpenseBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
